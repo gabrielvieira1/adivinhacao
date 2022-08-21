3500c7b94b349e7bbc422dc2543fb1e58a3a92bb
Gabriel Vieira <gabrielvieira1@protonmail.com>
2022-08-20 22:50:50 -0300

    Condicionais e Loops com Ifs, Fors e Whiles

--- adivinhacao.c
+++ adivinhacao.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#define NUMERO_TENTATIVAS 5
+
 int main()
 {
     printf("******************************************\n");
@@ -8,10 +10,44 @@ int main()
 
     int numero_secreto = 42;
     int chute;
+    int tentativas = 1;
 
-    printf("Qual é o seu chute?\n");
-    scanf("%d", &chute);
-    printf("O Seu chute foi %d\n", chute);
+    while (1)
+    {
+        printf("Tentativa %d\n", tentativas);
+        printf("Qual é o seu chute?\n");
 
-    return 0;
+        scanf("%d", &chute);
+        printf("Você chutou %d\n", chute);
+
+        if (chute < 0)
+        {
+            printf("O número não pode ser negativo\n");
+            continue;
+        }
+
+        int acertou = chute == numero_secreto;
+        int maior = chute > numero_secreto;
+
+        if (acertou)
+        {
+            printf("Parabéns, você acertou!\n");
+            printf("Jogue de novo, você é um bom jogador!\n");
+
+            break;
+        }
+        else if (maior)
+        {
+            printf("Seu chute foi maior que o número secreto\n");
+        }
+        else
+        {
+            printf("O seu chute foi menor do que o número secreto.\n");
+        }
+
+        tentativas++;
+    }
+
+    printf("Fim do jogo\n");
+    printf("Você acertou em %d tentativas!\n", tentativas);
 }
